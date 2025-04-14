17abe9bd67eeaf76643cf94b98b38a074db93c8f
Cherletskiy <vcherletskiy@outlook.com>
2025-04-14 12:01:39 +0300

    JWT

--- news_blog/blog/permissions.py
+++ news_blog/blog/permissions.py
@@ -3,5 +3,9 @@ from rest_framework import permissions
 
 class IsOwnerOrAdmin(permissions.BasePermission):
     """Разрешение для владельца комментария или админа"""
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user or request.user.is_staff
--- news_blog/news_blog/urls.py
+++ news_blog/news_blog/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework_nested.routers import NestedDefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
 from blog.views import PostViewSet, CommentViewSet
 
 
@@ -16,5 +18,9 @@ posts_router.register(r'comments', CommentViewSet, basename='post-comments')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('api/v1/', include(posts_router.urls))
+    path('api/v1/', include(posts_router.urls)),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
